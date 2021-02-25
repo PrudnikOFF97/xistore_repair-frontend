3db041cd6d82d8389076eda1817a500e9b5db178
PrudnikOFF <prudnikoffvi@gmail.com>
2021-02-25 16:46:15 +0300

    fix: fix auth faild error

--- src/Components/AddModel/AddModel.js
+++ src/Components/AddModel/AddModel.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import { Button } from '@material-ui/core';
 import { useHistory } from "react-router-dom";
+import Axios from 'axios';
 
 
 
 
 const AddModel = () => {
+  const [message, setMessage] = useState('');
     const CssTextField = withStyles({
         root: {
           '& label.Mui-focused': {
@@ -24,14 +26,26 @@ const AddModel = () => {
         },
       })(TextField);
     const history = useHistory();
+    const submitHandler = (e) =>{
+      e.preventDefault();
+      let data = new FormData(e.target);
+      Axios.post(process.env.REACT_APP_API_URL+"/add-model", data)
+      .then(res => {
+        history.push("/");
+      })
+      .catch(res => {
+        setMessage(res.response.data.message);
+      })
+    };
 
     
     return (
-        <form action={process.env.REACT_APP_API_URL+"/add-model"} method="post" autoComplete="off" onSubmit={() => setTimeout(() =>history.push("/"))}>
+        <form autoComplete="off" onSubmit={(event) => submitHandler(event)}>
             <CssTextField margin="dense" required={true} style={{margin: "15px"}} color="secondary" name="name" variant="outlined" label="Наименование из 1С:"/>
             <CssTextField margin="dense" required={true} style={{margin: "15px"}} name="code" variant="outlined" label="Штрих-код:"/>
             <div></div>
             <Button type="submit" style={{margin: "15px"}} variant="contained" color="primary">Добавить номенклатуру</Button>
+            <h1>{message}</h1>
         </form>
     );
 }
